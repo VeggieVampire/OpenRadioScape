1621a0d85925fd9d6fa8df8c8ddb94571efdc4d0
Nick Farrow <VeggieVampire@users.noreply.github.com>
2023-10-23 02:58:06 -0500

    Update playOpenRadioScape.py

--- playOpenRadioScape.py
+++ playOpenRadioScape.py
@@ -1,9 +1,13 @@
 import pygame
 import rtlsdr
+import sys
 
 # Initialize Pygame and set up the game window
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
+
+# Set the window title
+pygame.display.set_caption("RTL-SDR Signal Detection")
 
 # Set up the player character
 player_image = pygame.image.load("player.png")
@@ -20,8 +24,22 @@ sdr.read_samples()
 world_image = pygame.image.load("world.png")
 world_rect = world_image.get_rect()
 
-# Set up the "SDR zone" where the player can use the RTL-SDR
-sdr_zone_rect = pygame.Rect(200, 200, 400, 200)
+# Set up the "SDR zone" image
+sdr_zone_image = pygame.image.load("SDR_radio.png")
+sdr_zone_rect = sdr_zone_image.get_rect()
+sdr_zone_rect.move_ip(150,110)
+
+# Define a function to do something with the SDR data
+def do_something_with_sdr_data(sdr_data):
+    # You can add your own code here to process the SDR data and affect the game state
+    print(sdr_data)
+
+# Initialize the joystick
+pygame.joystick.init()
+
+# Create a joystick object
+joystick = pygame.joystick.Joystick(0)
+joystick.init()
 
 # Main game loop
 while True:
@@ -30,16 +48,14 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-        elif event.type == pygame.KEYDOWN:
-            # Move the player character based on key presses
-            if event.key == pygame.K_LEFT:
-                player_rect.x -= 5
-            elif event.key == pygame.K_RIGHT:
-                player_rect.x += 5
-            elif event.key == pygame.K_UP:
-                player_rect.y -= 5
-            elif event.key == pygame.K_DOWN:
-                player_rect.y += 5
+
+    # Get the x and y axis values from the joystick
+    x_axis = joystick.get_axis(0)
+    y_axis = joystick.get_axis(1)
+
+    # Move the player character based on joystick input
+    player_rect.x += int(x_axis * 5)
+    player_rect.y += int(y_axis * 5)
 
     # Check if the player is in the "SDR zone"
     if player_rect.colliderect(sdr_zone_rect):
@@ -47,7 +63,10 @@ while True:
         sdr_data = sdr.read_samples(1024)
         do_something_with_sdr_data(sdr_data)
 
-    # Render the game world and player character to the screen
+
+    # Render the game world, "SDR zone", and player character to the screen
     screen.blit(world_image, world_rect)
+    screen.blit(sdr_zone_image, sdr_zone_rect)
     screen.blit(player_image, player_rect)
     pygame.display.flip()
+
